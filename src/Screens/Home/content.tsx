09b3977fff7e66d467104b09da7dbebd7ab66bf2
Daniel Alzate <danielalza01@gmail.com>
2021-03-15 11:46:53 -0500

    feat(home): add search field for product

--- src/Screens/Home/content.tsx
+++ src/Screens/Home/content.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TextInput,
   FlatList
 } from 'react-native';
 import { getCategories } from '../../Services';
@@ -23,9 +24,22 @@ export default class HomeScreen extends Component<HomeScreenProps, HomeScreenSta
     getCategories().then(categories => this.setState({ categories }))
   }
 
+  _onBlur = () => {
+    this.props.navigation.navigate('Items', { search: this.state.search })
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={search => this.setState({ search })}
+            onBlur={() => this._onBlur()}
+            placeholder="Buscar productos, marcas y más...">
+          </TextInput>
+        </View>
+
         <FlatList
           ListHeaderComponent={<Text style={styles.ListHeader}>{'Categorías'}</Text>}
           data={this.state.categories}
